8af86df7a3705b3a4485d95566c8fdc044da04de
Johnny Shields <johnny.shields@gmail.com>
2014-04-05 07:06:45 +0900

    Merge the functionality of https://github.com/sportngin/okcomputer-newrelic-ignore into the `OkComputer::Engine` initializer. There's no practical case where a NewRelic user would want NewRelic to count OkComputer.

--- lib/okcomputer/engine.rb
+++ lib/okcomputer/engine.rb
@@ -5,6 +5,7 @@ module OKComputer
 
     config.after_initialize do |app|
       OKComputer::Engine.prepend_routes(app)
+      OKComputer::Engine.ignore_newrelic
     end
 
     config.generators do |g|
@@ -13,13 +14,24 @@ module OKComputer
 
     private
 
-    # Private: Prepend routes so a catchall doesn't get in the way
+    # Private: Prepend OKComputer routes so a catchall doesn't get in the way
     def self.prepend_routes(app)
       return if OKComputer::Engine.routes.recognize_path('/') rescue nil
       require OKComputer::Engine.root.join("app/controllers/o_k_computer/ok_computer_controller")
 
       app.routes.prepend do
         mount OKComputer::Engine => OKComputer.mount_at, as: "okcomputer"
+      end
+    end
+
+    # Private: If NewRelic is installed, inject OkComputerController with logic to ignore
+    # it for the purposes of NewRelic app timing
+    def self.ignore_newrelic
+      if defined?(NewRelic::Agent::Instrumentation::ControllerInstrumentation)
+        OKComputer::OkComputerController.class_eval do
+          include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+          newrelic_ignore
+        end
       end
     end
   end
--- spec/okcomputer/engine_spec.rb
+++ spec/okcomputer/engine_spec.rb
@@ -17,4 +17,25 @@ describe OKComputer::Engine do
       routes.should include '/foo'
     end
   end
+
+  context "#ignore_newrelic" do
+
+    context "#when NewRelic is installed" do
+      before do
+        stub_const('NewRelic::Agent::Instrumentation::ControllerInstrumentation', Module.new)
+      end
+
+      it "should inject newrelic_ignore" do
+        OKComputer::OkComputerController.should_receive(:newrelic_ignore).with(no_args())
+        OKComputer::Engine.send(:ignore_newrelic)
+      end
+    end
+
+    context "#when NewRelic is not installed" do
+      it "should not inject newrelic_ignore" do
+        OKComputer::Engine.should_not_receive(:newrelic_ignore)
+        expect{ OKComputer::Engine.send(:ignore_newrelic) }.to_not raise_error
+      end
+    end
+  end
 end
