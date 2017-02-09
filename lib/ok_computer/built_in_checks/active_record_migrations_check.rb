3a119d40374561c83d86a111d2bbfa2acacd13da
David Hagege <david.hagege@gmail.com>
2017-02-09 12:38:37 +0900

    ActiveRecordMigrationsCheck now supports ActiveRecord < 4

--- lib/ok_computer/built_in_checks/active_record_migrations_check.rb
+++ lib/ok_computer/built_in_checks/active_record_migrations_check.rb
@@ -2,11 +2,22 @@ module OkComputer
   class ActiveRecordMigrationsCheck < Check
     # Public: Check if migrations are pending or not
     def check
-      if ActiveRecord::Migrator.needs_migration?
+      if needs_migration?
         mark_failure
         mark_message "Pending migrations"
       else
         mark_message "NO pending migrations"
+      end
+    end
+
+    private
+
+    def needs_migration?
+      if ActiveRecord::Migrator.respond_to?(:needs_migration?)
+        return ActiveRecord::Migrator.needs_migration?
+      else
+        (ActiveRecord::Migrator.migrations(ActiveRecord::Migrator.migrations_paths).collect(&:version) -
+         ActiveRecord::Migrator.get_all_versions(ActiveRecord::Base.connection)).size > 0
       end
     end
   end
--- spec/ok_computer/built_in_checks/active_record_migrations_check_spec.rb
+++ spec/ok_computer/built_in_checks/active_record_migrations_check_spec.rb
@@ -7,22 +7,55 @@ module OkComputer
     end
 
     context "#check" do
-      context "with no pending migrations" do
-        before do
-          expect(ActiveRecord::Migrator).to receive(:needs_migration?).and_return(false)
+      context "if activerecord supports needs_migrations?" do
+        context "with no pending migrations" do
+          before do
+            expect(ActiveRecord::Migrator).to receive(:needs_migration?).and_return(false)
+          end
+
+          it { should be_successful }
+          it { should have_message "NO pending migrations" }
         end
 
-        it { should be_successful }
-        it { should have_message "NO pending migrations" }
+        context "with pending migrations" do
+          before do
+            expect(ActiveRecord::Migrator).to receive(:needs_migration?).and_return(true)
+          end
+
+          it { should_not be_successful }
+          it { should have_message "Pending migrations" }
+        end
       end
 
-      context "with pending migrations" do
-        before do
-          expect(ActiveRecord::Migrator).to receive(:needs_migration?).and_return(true)
+      context "if ActiveRecord doesn't support needs_migratons?" do
+        context "with no pending migrations" do
+          before do
+            versions = [instance_double('version', :version => '20160510232542')]
+            expect(ActiveRecord::Migrator).to receive_messages(
+              respond_to?: false,
+              get_all_versions: ['20160510232542'],
+              migrations: versions
+            )
+          end
+
+          it { should be_successful }
+          it { should have_message "NO pending migrations" }
         end
 
-        it { should_not be_successful }
-        it { should have_message "Pending migrations" }
+        context "with pending migrations" do
+          before do
+            versions = [instance_double('version', :version => '20160510232542')]
+
+            expect(ActiveRecord::Migrator).to receive_messages(
+              respond_to?: false,
+              get_all_versions: [],
+              migrations: versions
+            )
+          end
+
+          it { should_not be_successful }
+          it { should have_message "Pending migrations" }
+        end
       end
     end
   end
